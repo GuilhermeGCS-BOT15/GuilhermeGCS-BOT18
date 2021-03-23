0ae7439d41b3a71c3386b6e27ac85b76638d42a8
GuilhermeGCS <guilhermegcs222@gmail.com>
2021-03-23 02:11:25 -0300

    First commit

---
+++ numowner.js
@@ -0,0 +1,12 @@
+const ownerNumber = ["5577997029532@s.whatsapp.net"]
+const OriginalOwner = '5577997029532'
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN:Meu criador^~^\n' 
+            + 'ORG:Ian;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=5577997029532:+55 77 99702-9532\n' 
+            + 'END:VCARD'
+
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
