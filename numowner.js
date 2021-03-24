dd63511f6c041d8d38748bd2e8c23bed92f1675e
GuilhermeGCS-BOT15 <79100010+GuilhermeGCS-BOT15@users.noreply.github.com>
2021-03-24 19:11:00 -0300

    Update numowner.js

--- numowner.js
+++ numowner.js
@@ -1,12 +1,12 @@
-const ownerNumber = ["5577997029532@s.whatsapp.net"]
-const OriginalOwner = '5577997029532'
-const vcard = 'BEGIN:VCARD\n' 
+const  ownerNumber  = [ "557797029532@s.whatsapp.net" ]
+const  OriginalOwner  =  '557797029532'
+const  vcard  =  'BEGIN: VCARD \ n' 
             + 'VERSION:3.0\n' 
             + 'FN:Meu criador^~^\n' 
             + 'ORG:Ian;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=5577997029532:+55 77 99702-9532\n' 
-            + 'END:VCARD'
+            +      'TEL; tipo = CELL; tipo = VOZ; waid = 557797029532: +55 77 9702-9532 \ n
+            +    'END: VCARD'
 
 exports.vcard = vcard
 exports.OriginalOwner = OriginalOwner
-exports.ownerNumber = ownerNumber
+exportações . ownerNumber  =  ownerNumber
